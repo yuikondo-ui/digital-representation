4cf249d2dbfb276832dbeb849bce51fc3d6c099b
Yui Kondo <yuikondobook@gmail.com>
2022-05-09 14:13:46 +0800

    Add user  model

---
+++ db/migrate/20220509061314_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :can
+      t.integer :id_number
+      t.string :country_code
+      t.integer :date_of_birth
+      t.string :gender
+      t.string :place_of_birth
+      t.integer :idd
+
+      t.timestamps
+    end
+  end
+end
